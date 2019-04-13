32bfcc145fc9af2a65255a9ceea313c686f22c1b
keithdadkins <keithdadkins@me.com>
2019-04-13 11:22:02 -0500

    improve aws-cli test failure output

--- packer/build_aws_cli_image.sh
+++ packer/build_aws_cli_image.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 # Builds an aws-cli docker image that we can use during builds
 
+failed_test(){
+    echo "aws-cli was unable to connect to aws successfully... "
+    echo "$1"
+    echo "Exiting."
+    exit 1
+}
+
 # build image
 sudo docker build -f aws-cli.Dockerfile -t aws-cli .
 
 # test aws-cli
 ver="$(sudo docker run --rm -e ${AWS_ACCESS_KEY_ID} -e ${AWS_SECRET_ACCESS_KEY} aws-cli sts get-caller-identity > /dev/null 2>&1)"
-[ $? -eq 0 ] && echo "aws-cli docker file tested successfully" || exit 1
+[ $? -eq 0 ] && echo "aws-cli docker file tested successfully" || failed_test $ver
