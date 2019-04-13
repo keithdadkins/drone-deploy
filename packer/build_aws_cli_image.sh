ae64ad56bd8adb183fb87b92366ce0959c948ef8
keithdadkins <keithdadkins@me.com>
2019-04-13 11:42:30 -0500

    check for aws credentials before attempting to build aws-cli, improve error handeling

--- packer/build_aws_cli_image.sh
+++ packer/build_aws_cli_image.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # Builds an aws-cli docker image that we can use during builds
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-0}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-0}
+
+# fail if credentials are not set
+[ "${AWS_ACCESS_KEY_ID}" -eq "0" ] && echo "no AWS_ACCESS_KEY_ID found.. exiting." && exit 1
+[ "${AWS_SECRET_ACCESS_KEY}" -eq "0" ] && echo "no AWS_SECRET_ACCESS_KEY found.. exiting." && exit 1
 
 failed_test(){
-    echo "aws-cli was unable to connect to aws successfully... "
-    echo "$1"
+    echo "aws-cli was unable to connect to aws successfully."
+    echo "When running 'aws sts get-caller-identity', the response given was: "
+    result=$(sudo docker run --rm -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" aws-cli sts get-caller-identity)
+    echo $result
     echo "Exiting."
     exit 1
 }
@@ -12,5 +20,5 @@ failed_test(){
 sudo docker build -f aws-cli.Dockerfile -t aws-cli .
 
 # test aws-cli
-ver="$(sudo docker run --rm -e ${AWS_ACCESS_KEY_ID} -e ${AWS_SECRET_ACCESS_KEY} aws-cli sts get-caller-identity > /dev/null 2>&1)"
-[ $? -eq 0 ] && echo "aws-cli docker file tested successfully" || failed_test $ver
+sudo docker run --rm -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" aws-cli sts get-caller-identity > /dev/null 2>&1
+[ $? -eq 0 ] && echo "aws-cli docker file tested successfully" || failed_test
