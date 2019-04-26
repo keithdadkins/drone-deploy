7b9ccffb07b5dbaff1534a8e630206259797f3f2
keithdadkins <keithdadkins@me.com>
2019-04-26 17:15:20 -0500

    ignore invalid pep8 gripes

--- cli/drone_deploy/__init__.py
+++ cli/drone_deploy/__init__.py
@@ -1,6 +1,6 @@
-from .deployment import Deployment
-from .bootstrap_cli import bootstrap
-from .list_cli import list_deployments
-from .new_deployment_cli import new_deployment
-from .deploy_cli import deploy
-from .show_cli import show
+from .deployment import Deployment               # noqa
+from .bootstrap_cli import bootstrap             # noqa
+from .list_cli import list_deployments           # noqa
+from .new_deployment_cli import new_deployment   # noqa
+from .deploy_cli import deploy                   # noqa
+from .show_cli import show                       # noqa
