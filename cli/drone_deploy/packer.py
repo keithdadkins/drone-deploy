27fcffb1dd3a9ecc87e063f92bef3683c5770609
keithdadkins <keithdadkins@me.com>
2019-04-28 23:17:07 -0500

    wrap packer commands into a class

--- cli/drone_deploy/packer.py
+++ cli/drone_deploy/packer.py
@@ -1,5 +1,7 @@
+import os
 import sys
 import subprocess
+from pathlib import Path
 
 
 class Packer():
@@ -16,14 +18,15 @@ class Packer():
 
 
     """
-    def __init__(self, working_dir):
+    def __init__(self, working_dir, packer_vars=[]):
         self.working_dir = working_dir
+        self.packer_vars = packer_vars
         self.__use_local_cmd()
 
     def __use_local_cmd(self):
         # use local packer command
         def packer(command):
-            command = f"packer {command}"
+            command = f"packer {command} {self.packer_vars}"
             p = subprocess.Popen(command, stderr=subprocess.PIPE, shell=True, text=True,
                                  cwd=self.working_dir)
             while True:
@@ -36,9 +39,36 @@ class Packer():
         self.packer = packer
 
     def __use_docker_cmd(self):
-        '''TODO: Use docker to run terraform.'''
+        '''TODO: Use docker to run packer.'''
         pass
+
+    @property
+    def packer_vars(self):
+        """Returns formatted vars ready for packer cmd ex. -var foo=bar -var biz=baz"""
+        output = ' '.join(["-var {0}={1}".format(k, v) for k, v in self.__packer_vars.items()])
+        return output
+
+    @packer_vars.setter
+    def packer_vars(self, packer_vars):
+        # unpacks packers vars (a list of tuples) into a dict
+        self.__packer_vars = {k: v for k, v in packer_vars}
 
     def build_ami(self):
         '''Builds the ami for the deployment.'''
-        pass
+        # Just run the build-drone-server-ami.sh script
+        # TODO: bring this all into python, or refactor build script to only accept
+        # command line args instead of env vars.
+
+        # cd into the deployment folder and run the build script
+        build_dir = Path(self.working_dir.parent.resolve())
+        command = "./build-drone-server-ami.sh -p"
+
+        p = subprocess.Popen(command, stderr=subprocess.PIPE, shell=True, text=True,
+                             cwd=build_dir)
+        while True:
+            out = p.stderr.read(1)
+            if out == '' and p.poll() is not None:
+                break
+            if out != '':
+                sys.stdout.write(out)
+                sys.stdout.flush()
