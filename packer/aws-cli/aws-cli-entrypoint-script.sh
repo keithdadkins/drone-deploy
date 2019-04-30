2f2b71bbb354159485b9e060c7ad25f683dfc256
keithdadkins <keithdadkins@me.com>
2019-04-30 17:09:10 -0500

    remove old packer dir (now in templates)

--- packer/aws-cli/aws-cli-entrypoint-script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ "$1" == "aws" ]; then
-    shift
-    exec aws "$@"
-fi
-exec "$@"
--- packer/build-scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-printf "\n\n\n\n***** CLEANING UP *****\n\n\n\n"
-
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y autoremove
-sudo apt-get -y clean
-sudo rm -rf /tmp/*
-history -c
