7de211f29cab2a53cb24c8127ca601e8452b1079
keithdadkins <keithdadkins@me.com>
2019-07-26 16:26:18 -0500

    add autocomplete and delete directory option to destroy

--- cli/drone_deploy/destroy_cli.py
+++ cli/drone_deploy/destroy_cli.py
@@ -1,12 +1,29 @@
 import click
+import shutil
 from pathlib import Path
 from drone_deploy.deployment import Deployment
 
+
+def list_deployments(ctx, args, incomplete):
+    """
+    Display list of deployments for bash autocompletion
+    """
+    deployments = []
+    try:
+        deployment_dir = Path.cwd().joinpath('deployments').resolve()
+        for deployment in deployment_dir.glob(f'{incomplete}*'):
+            if deployment.is_dir():
+                deployments.append(click.echo(deployment.name))
+        return [k for k in deployments if incomplete in k]
+    except Exception:
+        return ""
+
 # $> drone-deploy deploy [deployment name]
 @click.group(invoke_without_command=True)
-@click.argument('deployment_name')
+@click.argument('deployment_name', type=click.STRING, autocompletion=list_deployments)
+@click.option('--rm/--no-rm', default=False)
 @click.pass_obj
-def destroy(aws, deployment_name):
+def destroy(aws, deployment_name, rm):
     """
     !!! Destroys (runs 'terraform destroy') on <deployment_name> resources.
 
@@ -30,3 +47,7 @@ def destroy(aws, deployment_name):
     deployment = Deployment(deployment_dir)
     click.echo(f"Destroying {deployment_name}...")
     deployment.destroy()
+
+    if rm:
+        # delete the directory
+        shutil.rmtree(deployment_dir.parent)
