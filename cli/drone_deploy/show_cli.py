dd36c8d476124f96ce0d062f2150facd09d4ac3e
keithdadkins <keithdadkins@me.com>
2019-07-31 17:25:30 -0500

    fix typo

--- cli/drone_deploy/show_cli.py
+++ cli/drone_deploy/show_cli.py
@@ -26,7 +26,7 @@ def show(deployment_name):
     """
     deployment_dir = Path.cwd().joinpath('deployments', deployment_name, 'config.yaml').resolve()
     if not deployment_dir.exists():
-        click.echo("Couldn't find the deployment."
+        click.echo("Couldn't find the deployment. "
                    "Run 'drone-deploy list' to see available deployemnts.")
         return False
 
