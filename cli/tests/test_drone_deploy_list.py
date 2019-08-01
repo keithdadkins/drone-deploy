fe84951db6734fcf84db22e737f540ed519b2b2b
keithdadkins <keithdadkins@me.com>
2019-07-31 21:57:03 -0500

    test drone-deploy edit
    verify that only one syscall is made

--- cli/tests/test_drone_deploy_list.py
+++ cli/tests/test_drone_deploy_list.py
@@ -1,8 +1,8 @@
 from cli import cli
 
 
-def test_cli_list(new_deployment):
+def test_cli_list(runner, new_deployment):
     '''drone-deploy list'''
-    # result = runner.invoke(cli, ["list"])
-    assert new_deployment.exit_code == 0, "Error running 'drone-deploy list'"
-    assert 'foo' in new_deployment.output, "'drone-deploy list' did not contain a deployment 'drone.acme.com.'"
+    result = runner.invoke(cli, ["list"])
+    assert result.exit_code == 0, "Error running 'drone-deploy list'"
+    assert 'foo' in new_deployment.output, "'drone-deploy list' did not contain the expected new_deployment."
