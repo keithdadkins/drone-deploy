dcd9801f8accacd42610cc702ed79e016d26f77e
keithdadkins <keithdadkins@me.com>
2019-07-31 22:00:51 -0500

    noqa for mocks

--- cli/tests/test_drone_deploy_edit.py
+++ cli/tests/test_drone_deploy_edit.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def test_cli_edit(runner, new_deployment, mocker):
     '''drone-deploy show'''
     mocker.patch('os.system')
-    result = runner.invoke(cli, ["edit", "foo"])
-    call_list = f'{os.system.call_args}'
-    assert os.system.call_count == 1, 'drone-deploy edit should only make one syscall to edit config.yaml'
+    runner.invoke(cli, ["edit", "foo"])
+    call_list = f'{os.system.call_args}'    # noqa
+    assert os.system.call_count == 1, 'drone-deploy edit should only make one syscall to edit config.yaml' # noqa
     assert 'config.yaml' in call_list, 'drone-deploy edit did not attempt to open config.yaml file with an editor.'
