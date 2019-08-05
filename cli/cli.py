a5376a6f7506d552f488bf2cb257ad18a0ebe848
keithdadkins <keithdadkins@me.com>
2019-08-05 14:33:05 -0500

    remove unused aws boto3 stuff

--- cli/cli.py
+++ cli/cli.py
@@ -1,9 +1,6 @@
 import os
 import sys
 import click
-import boto3
-import boto3.session
-import botocore.exceptions
 from version import __version__
 from pathlib import Path
 from dotenv import load_dotenv
@@ -13,77 +10,14 @@ from drone_deploy import new_deployment, prepare_deployment,\
                          show_agent_command
 
 
-def aws_session(aws_access_key_id=None, aws_secret_access_key=None, aws_session_token=None,
-                profile_name=None):
-    '''
-    Returns a boto3 session we can pass around using click's context object. This is the session
-    that the app and api uses to communicate with AWS. Notes:
-        - Uses access keys over profile if available.
-        - Uses callers region if region_name is not set.
-    '''
-    session = boto3.session.Session()
-
-    # try to get a session using aws access keys first
-    if aws_access_key_id or aws_access_key_id:
-        try:
-            session = boto3.session.Session(aws_access_key_id, aws_secret_access_key,
-                                            aws_session_token)
-        except Exception as e:
-            print(type(e))
-            print(e.args)
-            print(e)
-            sys.exit("Exiting.")
-        return session
-
-    # else try using a profile
-    else:
-        try:
-            session = boto3.session.Session(profile_name=profile_name)
-        except botocore.exceptions.ProfileNotFound as err:
-            print(str(err), file=sys.stderr)
-            if session.available_profiles:
-                print(f"'{profile_name}' was not found. Verify the profile name or try with"
-                      "one of the following available profiles: {session.available_profiles}",
-                      file=sys.stderr)
-        except Exception as e:
-            print(type(e))
-            print(e.args)
-            print(e)
-            sys.exit("Exiting.")
-        return session
-
-
 # $> drone-deploy
 @click.group()
-@click.option('--region', '-r', help="The AWS region for the drone deployment.")
 @click.pass_context
-def cli(ctx, region):
+def cli(ctx):
     """
     Deploys and manages Drone CI/CD deployments on AWS.
-
-    AWS access keys or profiles can be used; however one or the other must be configured and
-    an aws region must set in order to make requests.
     """
-
-    # load aws creds from env vars if available
-    profile = os.getenv("AWS_DEFAULT_PROFILE", "default")
-    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    aws_session_token = os.getenv("AWS_SESSION_TOKEN")
-    if region is None:
-        region = os.getenv("DRONE_REGION", os.getenv('AWS_REGION'))
-
-    # create an aws session and set it's region. We will pass this around the cli app
-    # in the 'ctx' context object.
-    if ctx.obj is None:
-        ctx.obj = dict()
-
-    ctx.obj['aws'] = aws_session(profile_name=profile,
-                                 aws_access_key_id=aws_access_key_id,
-                                 aws_secret_access_key=aws_secret_access_key,
-                                 aws_session_token=aws_session_token)
-    ctx.obj['aws'].region = region
-
+    pass
 
 @cli.command()
 def version():
--- cli/tests/test_drone_deploy.py
+++ cli/tests/test_drone_deploy.py
@@ -1,8 +1,6 @@
 import os
-import boto3
 from packaging import version
-from cli import cli, aws_session
-
+from cli import cli
 
 def test_cli_smoke_test(runner):
     '''smoke test'''
@@ -14,14 +12,3 @@ def test_cli_smoke_test(runner):
 def test_cli_version(runner):
     result = runner.invoke(cli, ["version"])
     assert version.parse(result.output) > version.parse("0.0.1"), 'drone-deploy version should return a valid version greater than 0.0.1'
-
-
-def test_aws_session_ctx(runner):
-    '''test aws session'''
-    os.environ["AWS_SESSION"] = 'us-east-5'
-    aws_access_key_id = '1234567890'
-    aws_secret_access_key = 'ABCDE1234567890'
-    aws = aws_session(aws_access_key_id=aws_access_key_id,
-                      aws_secret_access_key=aws_secret_access_key,
-                      aws_session_token='')
-    assert type(aws) is boto3.session.Session, 'aws'
